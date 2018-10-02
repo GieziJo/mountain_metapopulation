3f64a82775d41b1e6db8e5435ee53153098248cf
giezenda <jonathan.giezendanner@epfl.ch>
2018-10-02 17:37:11 +0200

    added example data (OCN.tif, parameters.csv), and changed the main routine so that it can read most tif files. Adapted the main routine to read parameters from a csv file

--- src/metapopulationSimulation.py
+++ src/metapopulationSimulation.py
@@ -1,15 +1,19 @@
 import metapopulation
 import numpy as np
 from PIL import Image
+import rasterio
+import pandas as pd
 
 metapop = metapopulation.Metapopulation()
 
-im = Image.open('../input/dem.tif')
-Z = np.array(im)
-
+# Read and set terrain
+s = rasterio.open('../input/dem.tif')
+Z = np.squeeze(s.read()[0,:,:])
 metapop.setTerrain(Z)
 
-s = metapopulation.Species(10, 1500.0, 5, 15, 0.1)
+# read and set parameters
+pm = pd.read_csv('../input/parameters.csv',header=None,index_col=0).transpose()
+s = metapopulation.Species(pm.sigma.values[0], pm.z_opt.values[0], pm.D.values[0], pm.c.values[0], pm.e.values[0])
 
 metapop.setSpecies(s)
 metapop.computeFitness()
