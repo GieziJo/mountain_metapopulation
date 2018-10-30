d354557def592342f25672ea724a780f1ea03c10
giezenda <jonathan.giezendanner@epfl.ch>
2018-10-30 14:44:02 +0100

    changed default parameters

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,6 @@
 # echo "setting conda env"
-# conda create --name envToRunCode python=3 numpy matplotlib scipy numba pillow
+# conda create --name envToRunCode python=3 numpy matplotlib scipy numba pillow rasterio pandas
 # source activate envToRunCode
+# conda install -c conda-forge rasterio
 echo "running metapop model"
 python metapopulationSimulation.py
